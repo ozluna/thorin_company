d063e245e5816b25cbf840fb047f123bb2260da0
Ozge <ozonel883@hotmail.com>
2020-07-15 11:16:46 +0000

    new addings

--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,15 +12,23 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", company=data)
 
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-    
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
